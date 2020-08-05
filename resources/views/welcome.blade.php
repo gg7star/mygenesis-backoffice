b3e1938b1e754d8e9983e19ecb700590b9fc8c17
goldenstar8080 <gdst8965@gmail.com>
2020-08-05 16:28:24 +0700

    fix on api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,7 +119,7 @@ $.ajax({
     processData: false,
     success: function (jobs) {
       alert(jobs.length);
-      alert(jobs[0]["id"]);
+      alert(jobs[0][0]["id"]);
       // for (let i = 0; i < jobs.length; i++) {
 
       // }
