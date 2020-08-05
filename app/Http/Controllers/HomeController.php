a62cde1cb3380eb888e713ff263553592e8f527b
goldenstar8080 <gdst8965@gmail.com>
2020-08-05 15:03:12 +0700

    fix on api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,6 +62,9 @@ class HomeController extends Controller
         // echo $res->getHeader('content-type');
         // $xml = simplexml_load_string($res->getBody());
         $xml = simplexml_load_string($res->getBody());
+        if ($xml->job){
+            echo "jobs are exist";
+        }
         $jobs_data = [];
         foreach ($xml->job as $job) {
           $jobs_data[]=[
